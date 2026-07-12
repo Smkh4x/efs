f6d6984dcc9d7036c15cf8212c4e3e23c6259acb
Smkh4x <youssefalmouna8@gmail.com>
2026-07-12 12:43:41 -0400

    save simple socket

---
+++ socket.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+int main() {
+    int userSocket = socket(AF_INET, SOCK_STREAM, 0);
+    // char req[] =
+    // " GET /admin HTTP/1.1\r\n"
+    // " host: localhost\r\n"
+    // "\r\n";
+    return 0;
+}
